814f195e79b538df10d7a9e6b028ccfc69ee39cd
Björn Richter <2268851+x3rAx@users.noreply.github.com>
2021-08-06 11:05:58 +0200

    initial commit

--- src/main.js
+++ src/main.js
@@ -2,15 +2,16 @@ import Vue from "vue";
 import { store } from "./store";
 import App from "./App";
 
+import "tailwindcss/tailwind.css";
 import "./assets/main.css";
 
 Vue.config.devtools = true;
 Vue.prototype.$store = store;
 Vue.prototype.$state = store.state;
+
 new Vue({
   el: "#app",
-  components: { App },
-  template: `<App />`,
+  render: (h) => h(App),
   data: {
     $store: store.state
   }
